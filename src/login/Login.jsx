2942aea7bcf534e4af2cb75f4baa63851be19a60
thenaseemahmad <naseemahmad244@gmail.com>
2023-11-09 14:40:39 +0530

    Added user auth using mongodb

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,15 +1,37 @@
-import "dotenv/config";
 import React, { useState } from "react";
 import mongoose from 'mongoose';
 import md5 from 'md5';
+import { Dbtrans } from "../db/Dbtrans";
 
 function Login() {
     const [emailText, setEmailText] = useState("");
     const [passText, setPassText] = useState("");
 
-    mongoose.connect("mongodb+srv://"+process.env.MONGODB_USER+":"+process.env.MONGODB_USER_PASS+"@cluster0.c9xlloe.mongodb.net/userDB?retryWrites=true&w=majority",{useNewUrlParser:true});
-    const userSchema = new mongoose.Schema({username:String, password:String});
-    const User = new mongoose.model('users',userSchema);
+
+
+    function AuthenticateUser(email, password) {
+        const dbtrans = new Dbtrans("japsinnaseem","kxTEUsWXdQnSBVLT");
+        const userfound = dbtrans.authenticateAUser(email,password);
+        // mongoose.connect("mongodb+srv://" + process.env.MONGODB_USER + ":" + process.env.MONGODB_USER_PASS + "@cluster0.c9xlloe.mongodb.net/userDB?retryWrites=true&w=majority", { useNewUrlParser: true });
+        // mongoose.connect("mongodb+srv://japsinnaseem:kxTEUsWXdQnSBVLT@cluster0.c9xlloe.mongodb.net/userDB?retryWrites=true&w=majority", { useNewUrlParser: true });
+        // const userSchema = new mongoose.Schema({ username: String, password: String });
+        // const User = new mongoose.model('users', userSchema);
+        // User.findOne({username:email}).then((foundUser)=>{
+        //     if(foundUser){
+        //         if(foundUser.password===md5(password)){
+        //             return "user found"
+                    
+        //         }
+        //         else{
+        //             return "Wrong password"
+                    
+        //         }
+        //     }
+        //     else{
+        //         return "User does not exist, Please register"
+        //     }
+        // });
+    };
 
     function handleEmailChange(event) {
         setEmailText(event.target.value);
@@ -21,7 +43,8 @@ function Login() {
 
     function handleLoginSubmitButton() {
         //Check if this user exist in db
-        
+        AuthenticateUser(emailText,passText);
+
     }
 
     return (<main className="form-signin w-100 m-auto">
